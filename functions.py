8c0493b2e670515386d4ddda03ab12a414bb3cb8
NoahEspi <97457166+NoahEspi@users.noreply.github.com>
2022-01-10 10:05:50 -0700

    Added files via upload
    Imported existing calculator files from existing replit.

---
+++ functions.py
@@ -0,0 +1,107 @@
+import os
+import time
+from getkey import getkey, keys
+
+#splits a string into a list of characters
+def split(word):
+  return list(word);
+
+
+# function that turns off the calculator
+def off(offVar):
+
+  offVar = True;
+  
+  os.system('clear');
+  print("           Powering off..");
+  time.sleep(1);
+  os.system('clear');
+
+  return offVar;
+  
+
+# clears the console screen
+def clear(clearVar):
+
+  os.system('clear');
+  print("Welcome to calculator. For a list of functions, type 'help'.\n\n");
+
+  clearVar = True;
+  return clearVar;
+
+
+# changes ever instance of "ans" in userInput into the previous answer given
+def ans(expression, previousAns):
+
+  ansCount = expression.lower().count('ans')
+
+  for i in range(ansCount):
+
+    ansLocation = expression.lower().find('ans');
+
+    expression = expression[0:ansLocation] + str(previousAns) + expression[ansLocation+3:];
+
+  return expression;
+
+
+def factorial():
+  pass
+
+
+# removes all spaces from userInput
+def removeSpaces(expression):
+
+  spaceCount = expression.count(" ");
+
+  for i in range(spaceCount):
+
+    spaceLocation = expression.find(" ");
+
+    expression = expression[0:spaceLocation] + "" + expression[spaceLocation+1:];
+
+  return expression;
+
+
+# displays the help menu 
+def help(helpVar):
+
+  helpVar = True;
+
+  os.system('clear');
+
+  print("""Here is a list of functions:
+Currently, some functions/processes may not work as intended.
+
+  
+Display functions:
+ help → lists all available functions
+ clear → clears the screen
+ off → turns the calculator off
+  
+Math functions:
+ + → addition
+ - → subtraction
+ * → multiplication
+ / → division
+ ** → exponent
+ % → returns the remainder of a division problem
+ ans → returns answer to previous equation
+ pi → returns pi to 16 digits
+ e → returns Euler's number
+ tan(number) → returns tangent of 'number'
+ cos(number) → returns cosine of 'number' 
+ sin(number) → returns sine of 'number'
+ sqrt(number) → returns the square root of 'number'
+ factorial(number) → returns the factorial of 'number'
+ 
+ """);
+
+  print("Press any key to return back to calculator.");
+
+  getkey();
+
+  os.system('clear');
+
+  print("Welcome to calculator. For a list of functions, type 'help'.\n\n");
+
+  return helpVar;
---
+++ main.py
@@ -0,0 +1,71 @@
+import math; from math import *; 
+import cmath;
+import warnings; from warnings import *
+import functions;
+import time;
+import os;
+from getkey import getkey, keys;
+
+print("Welcome to calculator. For a list of functions, type 'help'\n\n");
+
+
+off = False;
+
+warnings.filterwarnings("ignore");
+
+while not off:
+
+  fact = False;
+  helpMenu = False;
+  clearing = False;
+  Enter = False;
+
+  # takes input from user and makes sure no error occurs when they input "ctrl + c"
+  try:
+    userInput = input("");
+  except KeyboardInterrupt:
+    userInput = "off";
+
+  #removes spaces from equation
+  if " " in userInput.lower():
+      userInput = functions.removeSpaces(userInput);
+
+
+  #avoids returning an error if user hits enter
+  if userInput == "":
+    Enter = True;
+
+
+  #off button 
+  if userInput.lower() == "off":
+    off = functions.off(off);
+
+
+  #help menu
+  if userInput.lower() == "help":
+    helpMenu = functions.help(helpMenu);  
+          
+
+  #clear screen button
+  if userInput.lower() == 'clear':
+    clearing = functions.clear(clearing);
+    
+
+  #ans function
+  if 'ans' in userInput.lower():
+    
+    try:
+      userInput = functions.ans(userInput, prevAns);
+    except NameError:
+      userInput = "Oops, sowwy, that function doesn't workies uwu"
+
+  #final exec function that calculates userInput
+  if not off and not helpMenu and not clearing and not Enter:
+
+    try:
+      exec('y='+userInput)
+      print("               ", y, "\n-------------------------");
+      prevAns = y
+
+    except Exception:
+      print("           ERROR: SYNTAX\n-------------------------");
